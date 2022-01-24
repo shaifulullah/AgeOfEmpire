a1c3192c893e5cdeef572c6947b7476314396d09
Shaiful Ullah <smshaifulullah@gmail.com>
2022-01-24 08:38:20 -0500

    initial project

---
+++ AgeOfEmpire/Models/ArmyUnits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgeOfEmpire.Models
+{
+    public class ArmyUnits
+    {
+        public int health { get; set; }
+        public int armyid { get; set; }
+        public int hitpoints { get; set; }
+        public String name { get; set; }
+        public string imgsrc { get; set; }
+        public int id { get; set; }
+    }
+}
